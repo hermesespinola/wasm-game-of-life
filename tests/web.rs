16dbb45a7fe6a167c2e821689421c7b24fb05e37
Hermes <hermes.espinola@gmail.com>
2019-07-18 10:10:37 -0500

    remove pass test

--- tests/web.rs
+++ tests/web.rs
@@ -9,11 +9,6 @@ use wasm_game_of_life::Universe;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
-#[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
-}
-
 fn input_spaceship() -> Universe {
     let mut universe = Universe::new(6, 6);
     universe.empty_cells();
